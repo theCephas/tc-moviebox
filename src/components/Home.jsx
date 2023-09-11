3d6c390672ff351b6c8b67dbe822ca3b2beaae04
theCephas <oshoiseoluwa@gmail.com>
2023-09-11 08:46:23 +0100

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import { Helmet } from "react-helmet";
+
+
+export default function Home (){
+        return (
+                <Fragment>
+                        <Helmet>
+                                <title>MovieBox - Home</title>
+                        </Helmet>
+
+                        <div>
+
+                        </div>
+                        
+                </Fragment>
+        )
+}
